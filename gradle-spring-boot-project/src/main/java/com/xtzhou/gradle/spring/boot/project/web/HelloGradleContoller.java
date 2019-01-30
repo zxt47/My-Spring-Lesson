cb53ce738b307cfe0f17743469415a0d32b9e72a
xtzhou <zhouxiaotian47@yeah.net>
2019-01-30 23:03:03 +0800

    添加Redis配置
    如题

--- gradle-spring-boot-project/src/main/java/com/xtzhou/gradle/spring/boot/project/web/HelloGradleContoller.java
+++ gradle-spring-boot-project/src/main/java/com/xtzhou/gradle/spring/boot/project/web/HelloGradleContoller.java
@@ -1,8 +1,11 @@
 package com.xtzhou.gradle.spring.boot.project.web;
 
 
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  *
  */
+@RestController
 public class HelloGradleContoller {
 }
