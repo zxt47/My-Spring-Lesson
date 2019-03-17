93d65188d635afe67561dd7d10c5b5abb0907da7
xtzhou <zhouxiaotian47@yeah.net>
2019-03-17 17:30:06 +0800

    上传lombok插件

--- gradle-spring-boot-project/src/main/java/com/xtzhou/gradle/spring/boot/project/web/HelloGradleContoller.java
+++ gradle-spring-boot-project/src/main/java/com/xtzhou/gradle/spring/boot/project/web/HelloGradleContoller.java
@@ -1,11 +1,14 @@
 package com.xtzhou.gradle.spring.boot.project.web;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  *
  */
 @RestController
+@Slf4j
 public class HelloGradleContoller {
+
 }
